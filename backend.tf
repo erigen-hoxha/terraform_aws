0b02272fa5eff4aa63432b60b945b5c2b6c64951
erigen-hoxha <hoxha.erigen@gmail.com>
2024-10-07 00:53:28 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
+#This file configures the Terraform backend to store the state in an S3 bucket.
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-bucket"
-    key    = "global/s3/terraform.tfstate"
-    region = "us-east-1"
+    bucket = "my-terraform-state-bucket"    #the name of the S3 bucket where Terraform will store its state
+    key    = "global/s3/terraform.tfstate"  #the specific file path within the S3 bucket to store the state file
+    region = "us-east-1"                    #the region where the S3 bucket is located
   }
 }
