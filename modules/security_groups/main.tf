0055b5fb5422936974404ed094b379087e7dc4d9
erigen-hoxha <hoxha.erigen@gmail.com>
2024-10-07 01:21:49 +0200

    Update main.tf

--- modules/security_groups/main.tf
+++ modules/security_groups/main.tf
@@ -27,3 +27,13 @@ resource "aws_security_group" "rds_sg" {
     cidr_blocks = ["10.0.0.0/16"]
   }
 }
+
+resource "aws_security_group" "lb_sg" {
+  vpc_id = aws_vpc.main_vpc.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
