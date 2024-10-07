9765d6dc4e67717e3778ea4deeb4a3706645531a
erigen-hoxha <hoxha.erigen@gmail.com>
2024-10-07 13:34:17 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-#This file configures the Terraform backend to store the state in an S3 bucket.
+#This file configures the Terraform backend to store the state in an S3 bucket and DynamoDB Table for Lock State.
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-bucket"    #the name of the S3 bucket where Terraform will store its state
-    key    = "global/s3/terraform.tfstate"  #the specific file path within the S3 bucket to store the state file
-    region = "us-east-1"                    #the region where the S3 bucket is located
-    dynamodb_table = "terraform-state-lock-table"   # the DynamoDB table for state locking
+    bucket           = "my-terraform-state-bucket"        #the name of the S3 bucket where Terraform will store its state
+    key              = "global/s3/terraform.tfstate"      #the specific file path within the S3 bucket to store the state file
+    region           = "us-east-1"                        #the region where the S3 bucket is located
+    dynamodb_table   = "terraform-state-lock-table"       # the DynamoDB table for state locking
   }
 }
