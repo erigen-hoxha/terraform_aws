62dfdc0e1b8942bb55e786b48650f1bf6d7b7bbf
erigen-hoxha <hoxha.erigen@gmail.com>
2024-10-07 13:23:34 +0200

    Update variables.tf

--- modules/autoscaling/variables.tf
+++ modules/autoscaling/variables.tf
@@ -1,19 +1,39 @@
 variable "ec2_ami" {
-  default = "ami-xxxxxxxx"
+  description = "AMI ID for the EC2 instances"
+  default     = "ami-xxxxxxxx"
 }
 
 variable "ec2_instance_type" {
-  default = "t2.micro"
+  description = "Instance type for the EC2 instances"
+  default     = "t2.micro"
 }
 
 variable "asg_desired_capacity" {
-  default = 2
+  description = "Desired capacity of the Auto Scaling Group"
+  default     = 2
 }
 
 variable "asg_max_size" {
-  default = 3
+  description = "Maximum size of the Auto Scaling Group"
+  default     = 3
 }
 
 variable "asg_min_size" {
-  default = 1
+  description = "Minimum size of the Auto Scaling Group"
+  default     = 1
+}
+
+variable "cpu_high_threshold" {
+  description = "CPU utilization threshold for scaling up"
+  default     = 70
+}
+
+variable "cpu_low_threshold" {
+  description = "CPU utilization threshold for scaling down"
+  default     = 30
+}
+
+variable "cooldown_period" {
+  description = "Cooldown period for scaling policies (in seconds)"
+  default     = 300
 }
