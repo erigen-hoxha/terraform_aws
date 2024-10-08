611a244e19694df0ee56923ed267e664a122a998
erigen-hoxha <hoxha.erigen@gmail.com>
2024-10-08 13:34:29 +0200

    Update outputs.tf

--- modules/ecs_fargate/outputs.tf
+++ modules/ecs_fargate/outputs.tf
@@ -7,3 +7,13 @@ output "ecs_service_arn" {
   value = aws_ecs_service.api_service.arn
   description = "ARN of the ECS service"
 }
+
+output "ecs_cluster_id" {
+  value = aws_ecs_cluster.api_cluster.id
+  description = "ID of the ECS cluster"
+}
+
+output "task_definition_arn" {
+  value = aws_ecs_task_definition.api_task.arn
+  description = "ARN of the ECS task definition"
+}
