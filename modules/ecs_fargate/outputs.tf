18848f7519b91edacae3ed8a56a0e1a978fb9bab
erigen-hoxha <hoxha.erigen@gmail.com>
2024-10-08 13:34:10 +0200

    Update outputs.tf

--- modules/ecs_fargate/outputs.tf
+++ modules/ecs_fargate/outputs.tf
@@ -1,3 +1,9 @@
 output "ecs_service_name" {
   value = aws_ecs_service.api_service.name
+  description = "Name of the ECS service"
+}
+
+output "ecs_service_arn" {
+  value = aws_ecs_service.api_service.arn
+  description = "ARN of the ECS service"
 }
