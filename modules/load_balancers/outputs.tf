1427df0b04f9e1298b69290a9bec2954911a39d1
erigen-hoxha <hoxha.erigen@gmail.com>
2024-10-08 13:37:30 +0200

    Update outputs.tf

--- modules/load_balancers/outputs.tf
+++ modules/load_balancers/outputs.tf
@@ -1,9 +1,29 @@
 output "public_lb_dns" {
-  value = aws_lb.public_lb.dns_name
+  value       = aws_lb.public_lb.dns_name
   description = "DNS name of the public ALB"
 }
 
 output "internal_lb_dns" {
-  value = aws_lb.internal_lb.dns_name
+  value       = aws_lb.internal_lb.dns_name
   description = "DNS name of the internal ELB"
+}
+
+output "public_lb_arn" {
+  value       = aws_lb.public_lb.arn
+  description = "ARN of the public ALB"
+}
+
+output "internal_lb_arn" {
+  value       = aws_lb.internal_lb.arn
+  description = "ARN of the internal ELB"
+}
+
+output "public_lb_zone_id" {
+  value       = aws_lb.public_lb.zone_id
+  description = "Zone ID of the public ALB"
+}
+
+output "internal_lb_zone_id" {
+  value       = aws_lb.internal_lb.zone_id
+  description = "Zone ID of the internal ELB"
 }
